1277c96ddae1c730d6f758d41fc00fd68867b5ec
Stephen Whittle <swhittle@crypticwarrior.net>
2024-05-30 13:50:38 +1000

    2024.5.1
    Bugfixes
    
    * Updates `fmt` library to handle scenarios where RTTI is disabled and
      platforms that do not support POSIX threadsafe file IO extensions

--- modio/modio/detail/FmtWrapper.h
+++ modio/modio/detail/FmtWrapper.h
@@ -10,6 +10,20 @@
 
 #pragma once
 
+// Stubs for Posix extensions that are not available on some platforms, used by parts of FMT we don't use anyways
+#ifdef MODIO_USE_FMT_POSIX_STUBS
+inline void flockfile(FILE*) {}
+inline void funlockfile(FILE*) {}
+inline int getc_unlocked(FILE*)
+{
+	return -1;
+}
+inline int putc_unlocked(int, FILE*)
+{
+	return -1;
+}
+#endif
+
 #ifndef MODIO_USE_CUSTOM_FMT
 	#pragma push_macro("FMT_HEADER_ONLY")
 	#ifndef FMT_HEADER_ONLY
@@ -20,7 +34,7 @@
 #ifdef MODIO_PLATFORM_UNREAL
 
 	#include MODIO_UNREAL_PLATFORM_PREAMBLE
-	#pragma push_macro("check") 
+	#pragma push_macro("check")
 	#undef check
 	#if PLATFORM_WINDOWS || PLATFORM_XBOXONEGDK || PLATFORM_XSX || PLATFORM_XB1
 		#pragma warning(push)
@@ -35,6 +49,8 @@
 	#include "fmt/chrono.h"
 	#include "fmt/format.h"
 	#include "fmt/printf.h"
+	#include "fmt/ranges.h"
+	#include "fmt/xchar.h"
 	#if PLATFORM_WINDOWS || PLATFORM_XBOXONEGDK || PLATFORM_XSX || PLATFORM_XB1
 		#pragma warning(pop)
 	#endif
@@ -48,6 +64,8 @@
 	#include "fmt/chrono.h"
 	#include "fmt/format.h"
 	#include "fmt/printf.h"
+	#include "fmt/ranges.h"
+	#include "fmt/xchar.h"
 	#pragma pop_macro("_LIBCPP_VERSION")
 
 #endif
